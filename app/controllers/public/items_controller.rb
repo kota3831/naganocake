0679fa81d162b8045d01fc94a6d998b384caa829
kota3831 <171798512+kota3831@users.noreply.github.com>
2025-02-08 09:55:04 +0000

    権限の設定完了

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,9 @@
 class Public::ItemsController < ApplicationController
+  before_action :authenticate_customer!, except: [:index, :show]
+
+  def index
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
 
-  devise_for :costomers, skip: [:passwords], controllers: {
+  devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  namespace :customer do
+    resources :items, only: [:index, :show]
+  end
 
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
 
   root to: 'homes#top'
-  get 'homes/about'
+  get '/about' => 'homes#about'
 
-  resources :items, only: [:index, :show]
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
